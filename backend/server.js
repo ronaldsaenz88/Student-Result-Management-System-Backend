829c18ff91ec52b256f0e45b400edccb9e5e355a
Ronald Saenz <rjsaenz@espol.edu.ec>
2024-02-17 12:50:26 -0500

    Build to heroku

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ app.use(cors());
 app.use(express.json()); // Body parser
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 
 // Define routes here...
 
 // This route serves the React app
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "../build", "index.html")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
